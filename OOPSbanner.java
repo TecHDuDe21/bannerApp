0de8837c8c0c04e33ddb54df743840919fb92007
TecHDuDe21 <varunv2506@gmail.com>
2026-03-01 23:49:34 +0530

    UC5 code,adds for each loop and uses string.join()

--- OOPSbanner.java
+++ OOPSbanner.java
@@ -1,9 +1,9 @@
 public class OOPSbanner {
-    public static void main(String[] args) {int i;
-        String sentence[]={"   ***       ***    *******     *****  "," **   **   **   **  **    **  **    ** ","**     ** **     ** **     **  **      ","**     ** **     ** **    **    *****  ","**     ** **     ** *******         ** "," **   **   **   **  **         **   ** ","   ***       ***    **          *****  "};
-     for(i=0;i<7;i++)
-     {
-        System.out.println(sentence[i]);
+    public static void main(String[] args) {
+        String sentence[]={String.join(" ","   ***   ","   ***   ","*******  ","  *****  "),String.join(" "," **   ** "," **   ** ","**    ** "," **   ** "),String.join(" ","**     **","**     **","**     **","**       "),String.join(" ","**     **","**     **","**    ** ","  *****  "),String.join(" ","**     **","**     **","*******  ","      ** "),String.join(" "," **   ** "," **   ** ","**       "," **   ** "),String.join(" ","   ***   ","   ***   ","**       ","  *****  ")};
+     for (String i : sentence)
+	 {
+        System.out.println(i);
      }
    }
 }
