73349be0201d05ba453b109a1197ad68d863c01a
TecHDuDe21 <varunv2506@gmail.com>
2026-03-01 21:02:59 +0530

    UC4 code

---
+++ OOPSbanner.java
@@ -0,0 +1,9 @@
+public class OOPSbanner {
+    public static void main(String[] args) {int i;
+        String sentence[]={"   ***       ***    *******     *****  "," **   **   **   **  **    **  **    ** ","**     ** **     ** **     **  **      ","**     ** **     ** **    **    *****  ","**     ** **     ** *******         ** "," **   **   **   **  **         **   ** ","   ***       ***    **          *****  "};
+     for(i=0;i<7;i++)
+     {
+        System.out.println(sentence[i]);
+     }
+   }
+}
