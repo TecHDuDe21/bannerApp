82929d9626b3255838d97eb28452adadbe2a9115
TecHDuDe21 <varunv2506@gmail.com>
2026-03-02 00:54:43 +0530

    "UC 6 has helper methods added"

--- OOPSbanner.java
+++ OOPSbanner.java
@@ -1,9 +1,35 @@
 public class OOPSbanner {
-    public static void main(String[] args) {
-        String sentence[]={String.join(" ","   ***   ","   ***   ","*******  ","  *****  "),String.join(" "," **   ** "," **   ** ","**    ** "," **   ** "),String.join(" ","**     **","**     **","**     **","**       "),String.join(" ","**     **","**     **","**    ** ","  *****  "),String.join(" ","**     **","**     **","*******  ","      ** "),String.join(" "," **   ** "," **   ** ","**       "," **   ** "),String.join(" ","   ***   ","   ***   ","**       ","  *****  ")};
-     for (String i : sentence)
-	 {
-        System.out.println(i);
+   public static String[] Opattern(){
+    return new String[]{"   ***   ",
+                        " **   ** ",
+                        "**     **",
+                        "**     **",
+                        "**     **",
+                        " **   ** ",
+                        "   ***   "}; };
+   public static String[] Ppattern(){
+    return new String[]{"*******  ",
+                        "**    ** ",
+                        "**     **",
+                        "**    ** ",
+                        "*******  ",
+                        "**       ",
+                        "**       "}; };
+   public static String[] Spattern(){
+    return new String[]{"  *****  ",
+                        " **   ** ",
+                        "**       ",
+                        "  *****  ",
+                        "      ** ",
+                        " **   ** ",
+                        "  *****  "}; };
+
+    public static void main(String[] args) {int i;
+     String[] O=Opattern();
+     String[] P=Ppattern();
+     String[] S=Spattern();
+     for (i=0;i<O.length;i++){
+      System.out.println(O[i]+" "+O[i]+" "+P[i]+" "+S[i]);
      }
    }
 }
