282860fd41d6eec64a702d0306e9f28c86cff8c7
TecHDuDe21 <varunv2506@gmail.com>
2026-03-10 12:59:59 +0530

    UC 8

--- OOPSbanner.java
+++ OOPSbanner.java
@@ -1,107 +1,63 @@
-class CharacterPatternMap {
-    Character character;
-    String[] pattern;
-
-    // Constructor
-    public CharacterPatternMap(Character character, String[] pattern) {
-        this.character = character;
-        this.pattern = pattern;
-    }
-
-    // Getters
-    public Character getCharacter() {
-        return character;
-    }
-
-    public String[] getPattern() {
-        return pattern;
-    }
-}
-
-public class OOPSbanner {
-
-    // 2. Create method to initialize the array of CharacterPatternMap objects
-    public static CharacterPatternMap[] createCharacterPatternMaps() {
-        CharacterPatternMap[] charMaps = new CharacterPatternMap[4];
-        
-        String[] oPattern = {
-                        "   ***   ",
-                        " **   ** ",
-                        "**     **",
-                        "**     **",
-                        "**     **",
-                        " **   ** ",
-                        "   ***   "
-        };
-        
-        String[] pPattern = {
-                        "*******  ",
-                        "**    ** ",
-                        "**     **",
-                        "**    ** ",
-                        "*******  ",
-                        "**       ",
-                        "**       "
-        };
-        
-        String[] sPattern = {
-                        "  *****  ",
-                        " **   ** ",
-                        "**       ",
-                        "  *****  ",
-                        "      ** ",
-                        " **   ** ",
-                        "  *****  "
-        };
-
-        // Adding a space pattern to create gaps between our letters
-        String[] spacePattern = {
-            " ",
-            " ",
-            " ",
-            " ",
-            " ",
-            " ",
-            " "
-        };
-
-        // Populate the array
-        charMaps[0] = new CharacterPatternMap('O', oPattern);
-        charMaps[1] = new CharacterPatternMap('P', pPattern);
-        charMaps[2] = new CharacterPatternMap('S', sPattern);
-        charMaps[3] = new CharacterPatternMap(' ', spacePattern);
-
-        return charMaps;
-    }
-
-    // 3. Implement getCharacterPattern to retrieve the pattern for a given character
-    public static String[] getCharacterPattern(char ch, CharacterPatternMap[] charMaps) {
-        for (CharacterPatternMap map : charMaps) {
-            if (map.getCharacter() == ch) {
-                return map.getPattern();
-            }
-        }
-        // Fallback in case a character isn't found
-        return new String[]{"", "", "", "", "", "", ""}; 
-    }
-
-    // 4. Use printMessage to render the banner
-    public static void printMessage(String message, CharacterPatternMap[] charMaps) {
-        // Loop through each of the 7 lines vertically
-        for (int i = 0; i < 7; i++) {
-            // Loop through each character in the message horizontally
-            for (int j = 0; j < message.length(); j++) {
-                String[] pattern = getCharacterPattern(message.charAt(j), charMaps);
-                System.out.print(pattern[i]);
-            }
-            System.out.println(); // Move to the next line after finishing the row
-        }
-    }
+import java.util.HashMap;
+import java.util.Map;
+public class OOPSBannerUC8 {
 
     public static void main(String[] args) {
-        CharacterPatternMap[] maps = createCharacterPatternMaps();
+        Map<Character, String[]> patternMap = buildCharacterPatternMap();
+
+        String message = "OOPS";
+
+        displayBanner(message, patternMap);
+    }
+
+    private static Map<Character, String[]> buildCharacterPatternMap() {
+
+        Map<Character, String[]> map = new HashMap<>();
+
+        map.put('O', new String[] {
+            " ***** ",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            " ***** "
+        });
+
+        map.put('P', new String[] {
+            " ***** ",
+            "*     *",
+            "*     *",
+            " ***** ",
+            "*      ",
+            "*      ",
+            "*      "
+        });
+
+        map.put('S', new String[] {
+            " ***** ",
+            "*      ",
+            "*      ",
+            " ***** ",
+            "      *",
+            "      *",
+            " ***** "
+        });
+
+        return map;
+    }
+    private static void displayBanner(String message, Map<Character, String[]> patternMap) {
+
         
-        // Pass the string with spaces so the spacePattern is utilized
-        printMessage("O O P S", maps); 
+        for (int row = 0; row < 7; row++) {
+
+            StringBuilder lineBuilder = new StringBuilder();
+            for (char ch : message.toCharArray()) {
+                String[] pattern = patternMap.get(ch);
+                lineBuilder.append(pattern[row]).append(" ");
+            }
+
+            System.out.println(lineBuilder.toString());
+        }
     }
 }
